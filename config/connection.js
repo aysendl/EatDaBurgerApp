06135241cdca748f5cb37bd502199eedfc95ce95
Ayse <aysendl@gmail.com>
2020-08-02 21:15:55 -0700

    setting up db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
+var connection;
 
-const connection = mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    database:"burgers_db",
-    user:"root",
-    password:"password"
-});
 
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host:"localhost",
+        port:3306,
+        database:"burgers_db",
+        user:"root",
+        password:"password"
+    });
+}
 connection.connect((err)=>{
     if(err) throw err;
     console.log(`connect as ${connection.threadId}`);
